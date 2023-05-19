d57817bfef8d30080da9eefee921704976601e7a
nathansilvaa <122031636+nathansilvaa@users.noreply.github.com>
2023-05-19 13:56:51 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema")
+
+const body = document.querySelector("body")
+
+const imagenBotaoTrocaTema = document.querySelector(".imagen-botao")
+
+botaoAlterarTema.addEventListener("click",() => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro")
+
+    //body.classList.toggle("modo-escuro")
+    
+    if(modoEscuroEstaAtivo){
+        body.classList.remove("modo-escuro")
+        imagenBotaoTrocaTema.setAttribute("src","./src/imagens/sun.png")
+    } else {
+        body.classList.add("modo-escuro")
+        imagenBotaoTrocaTema.setAttribute("src","./src/imagens/moon.png")
+    }
+})
